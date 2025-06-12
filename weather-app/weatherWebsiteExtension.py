2e3671c77a37ab25eefcad3a71843387db021aed
ThemLemons <jimmyyo12@gmail.com>
2025-06-12 20:27:56 +1200

    pulls are now working

--- weather-app/weatherWebsiteExtension.py
+++ weather-app/weatherWebsiteExtension.py
@@ -1,17 +1,16 @@
-#weather cli
+#weather wesite Extension
 
 import argparse
 import json
 import requests
 import sys
+import streamlit as st
 from configparser import ConfigParser
 from urllib import parse, request
 from pprint import pp
 
+
 BASE_WEATHER_API_URL = "https://api.openweathermap.org/data/2.5/weather"
-PADDING = 20
-REVERSE = "\033[;7m"
-RESET = "\033[0m"
 
 def _get_api_key():
   config = ConfigParser()
@@ -44,36 +43,26 @@ def build_weather_query(city_input, imperial = False):
  url = (
  f"{BASE_WEATHER_API_URL}?q={city_input},"
  f"nz&appid={api_key}")
- #Making sure the API  URL is showing 
- #print(url)
  return url
 
-def get_weather_data(query_url):
- try:
- 	response = requests.get(query_url).json()
- except error.HTTPError:
-        sys.exit("Cant find weather Data for this city.")
- #data = response.read()
- #weather_apires = requests.get("").json()
- #pp(response) 
- return response
+def display_weather_info(weather_data):
+ first_L = weather_data['weather']
+ second_L = "Weather Condition: "+ first_L[0]['main'] + " Description: " +first_L[0]['description']
+ print(second_L)
+ parts = str(second_L)
+ return parts
 
-def display_weather_info(weather_data, imperial=False):
- city = weather_data["name"]
- weather_description = weather_data["weather"][0]["description"]
- temperature = weather_data["main"]["temp"]
- print(f"{REVERSE}{city:^{PADDING}}{RESET}", end="")
- print(f"\t{weather_description.capitalize():^{PADDING}}",end=" ",)
- print(f"({temperature}°{'F' if imperial else 'C'})")
+def create_website():
+ st.header("This is basic website", divider = "rainbow")
+ option = st.selectbox('Where are you looking for?',('Auckland','Tauranga','Wellington'))
+
+ api_key = _get_api_key() 
+ st.write("this is result", option)
+ urlss = build_weather_query(option)
+ data = requests.get(urlss).json()
+ splits = display_weather_info(data)
+ st.write(splits)
 
 if __name__ == "__main__":
-  read_user_cli_args()
 
-  city = sys.argv[1]
-  imperial = sys.argv[2]
-  #print(f"city: {city}" )
-  #print(f"imperial:{imperial}")
-  #print(city, imperial)
-  query_url = build_weather_query(city,imperial)
-  weather_data = get_weather_data(query_url)
-  display_weather_info(weather_data)
+	create_website()
